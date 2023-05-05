18678469ce30a1a117138d038dc8d207d73de99e
Haider <haider.nakara@gmail.com>
2023-05-05 10:55:13 +0530

    final

--- src/index.js
+++ src/index.js
@@ -29,9 +29,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
-// app.listen(process.env.PORT ?? 5000, () => {
-//   console.log("Server started at port " + process.env.PORT ?? 5000);
-//   mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },)
-// });
-
-export const handler = serverless(app);
+app.listen(process.env.PORT ?? 5000, () => {
+  console.log("Server started at port " + process.env.PORT ?? 5000);
+  mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },)
+});
